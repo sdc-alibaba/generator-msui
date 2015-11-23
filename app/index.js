d77e8fc502add86d46e9d58ab2b4c98b22d71cfc
半边 <zangtao.zt@alibaba-inc.com>
2015-11-23 23:32:19 +0800

    update generator logic

--- app/index.js
+++ app/index.js
@@ -1,12 +1,59 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var yeoman = require('yeoman-generator'),
+    fs = require('fs');
 
 module.exports = yeoman.generators.Base.extend({
-  a: function() {
-    this.log('aa')
-  },
-  b: function() {
-    this.log.ok(this.sourceRoot());
-  }
+    initializing: function() {
+        this.data = {}
+        this.log.ok(this.templatePath('./'))
+        this.log.ok(this.destinationPath())
+    },
+    prompting: function() {
+        var done = this.async();
+        this.prompt({
+            type    : 'input',
+            name    : 'product',
+            message : '请输入此份MSUI所属的上层业务线',
+            default : this.env.cwd.split('/').pop() // Default to current folder name
+        }, function (answers) {
+            this.log(answers);
+            this.data = ({
+                product: answers.product
+            })
+            done();
+        }.bind(this));
+
+    },
+    configuring: function() {
+
+    },
+    writing: function() {
+        var compilePath = this.templatePath('compile'),
+            files = fs.readdirSync(compilePath)
+
+        // 处理所有需要进行 EJS 模板编译的文件
+        for(var i = 0; i < files.length; i++) {
+            this.fs.copyTpl(
+                this.templatePath(compilePath),
+                this.destinationPath(),
+                this.data
+            )
+        }
+
+        // 不需要模板编译的目录copy，且文件内没有 EJS 语法字符串
+        this.directory(
+            this.templatePath('without_compile'),
+            this.destinationPath()
+        )
+
+        // 不需要模板编译的文件copy，因为文件内有 EJS 语法字符串，需要额外处理
+        this.fs.copy(
+            this.templatePath('Gruntfile.js'),
+            this.destinationPath('Gruntfile.js')
+        )
+    },
+    install: function() {
+        0&&this.npminstall();
+    }
 
 });
